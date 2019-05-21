344244a047bfd6d9cec722a55adf532318e27538
Satria Adi Putra <satadii11@gmail.com>
2019-05-21 16:53:32 +0700

    Add Common Classes

--- app/src/main/java/io/github/golok/biodata/common/BaseActivity.java
+++ app/src/main/java/io/github/golok/biodata/common/BaseActivity.java
@@ -4,17 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Satria Adi Putra
+ * 10116167
+ * IF-4
+ * Senin, 20 Mei 2019
+ */
 public abstract class BaseActivity extends AppCompatActivity {
-    public static final String EXTRA_BUNDLE_DATA = "extra::bundle";
-
     protected void show(Class destination) {
         Intent intent = new Intent(this, destination);
-        startActivity(intent);
-    }
-
-    protected void show(Class destination, Bundle extra) {
-        Intent intent = new Intent(this, destination)
-                .putExtra(extra.getString(EXTRA_BUNDLE_DATA), extra);
         startActivity(intent);
     }
 }
--- app/src/main/java/io/github/golok/biodata/common/BasePresenter.java
+++ app/src/main/java/io/github/golok/biodata/common/BasePresenter.java
@@ -1,5 +1,11 @@
 package io.github.golok.biodata.common;
 
+/**
+ * Satria Adi Putra
+ * 10116167
+ * IF-4
+ * Senin, 20 Mei 2019
+ */
 public interface BasePresenter {
     void start();
 }
